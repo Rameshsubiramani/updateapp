111c25faa911a440e05e6433ca8c6332bacc87a8
Rameshsubiramani <51048776+Rameshsubiramani@users.noreply.github.com>
2023-01-11 14:47:11 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -26,7 +26,7 @@ selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'],
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "container": {"padding": "0!important", "background-image": "/images.jpg"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "green"},
