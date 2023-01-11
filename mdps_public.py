22383e3ca5e9bf2019d9165e9604039ae71d1492
Rameshsubiramani <51048776+Rameshsubiramani@users.noreply.github.com>
2023-01-11 14:33:55 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -244,12 +244,6 @@ if (selected == "Parkinsons Prediction"):
 if (selected == 'Home'):
     # page title
    st.title('Multiple Disease Prediction System in ml') 
-   tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-   tab1.write("this is tab 1")
-   tab2.write("this is tab 2")
- #You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
   st.header(" What is Diabetes?")
   st.image('./image.jpg')
   col1, col2 ,col3,col4,col5,col6= st.columns(6)
