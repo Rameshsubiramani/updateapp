d4ae3df24dadd09365d53fb43b1ae9275e53fc51
Rameshsubiramani <51048776+Rameshsubiramani@users.noreply.github.com>
2023-01-11 13:50:52 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -38,9 +38,14 @@ if (selected == 'Home'):
     # page title
     st.title('Multiple Disease Prediction System in ml') 
     # Insert containers separated into tabs:
-    tab1, tab2 = st.tabs(["D1", "H1"])
-    tab1.write("Diabetics")
-    tab2.write("Heart attack")
+    tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+    tab1.write("this is tab 1")
+    tab2.write("this is tab 2")
+
+# You can also use "with" notation:
+   with tab1:
+   st.radio('Select one:', [1, 2])
+   
     st.header(" What is Diabetes?")
     st.image('./image.jpg')
     col1, col2 ,col3,col4,col5,col6= st.columns(6)
