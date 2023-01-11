30ea9653414b718d6cc39a5c9c825613187ba450
Rameshsubiramani <51048776+Rameshsubiramani@users.noreply.github.com>
2023-01-11 12:36:34 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -21,7 +21,7 @@ img = get_img_as_base64("image.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("");
+background-image: url("https://github.com/Rameshsubiramani/updateapp/blob/main/image.jpg");
 background-size: 180%;
 background-position: top left;
 background-repeat: no-repeat;
