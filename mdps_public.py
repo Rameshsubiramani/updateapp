3226f19b00bd0aa99806abe0ad01a4e3a7ee3bd0
Rameshsubiramani <51048776+Rameshsubiramani@users.noreply.github.com>
2023-01-11 12:39:28 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -4,44 +4,9 @@ Created on Sun May  8 21:01:15 2022
 
 @author: siddhardhan
 """
-import base64
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-img = get_img_as_base64("image.jpg")
-
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://github.com/Rameshsubiramani/updateapp/blob/main/image.jpg");
-background-size: 180%;
-background-position: top left;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
-</style>
-"""
-
 
 # loading the saved models
 
